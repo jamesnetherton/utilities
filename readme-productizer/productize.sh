f1eca2b36eb1145706aeed8570a9a2f0bc8e6844
James Netherton <jamesnetherton@gmail.com>
2017-08-31 10:56:33 +0100

    Add utility for tweaking README files

---
+++ readme-productizer/productize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+README=$1
+
+sed -i -e 's/WildFly Camel susbsystem/JBoss Fuse on EAP/g' \
+       -e 's/WildFly Camel susbsystem/JBoss Fuse on EAP/g' \
+       -e 's/WildFly Camel Subsystem/JBoss Fuse on EAP/g' \
+       -e 's/the wildfly-camel subsystem installed/JBoss Fuse installed/g' \
+       -e 's/standalone-full-camel.xml/standalone-full.xml/g' \
+       -e '/Learn more/,+20d' \
+    ${README}
