858d56ef1672d192946763a3c048dcae43f71d46
James Netherton <jamesnetherton@gmail.com>
2017-10-18 16:03:31 +0100

    More README replacements

--- readme-productizer/productize.sh
+++ readme-productizer/productize.sh
@@ -8,4 +8,5 @@ sed -i -e 's/WildFly Camel susbsystem/JBoss Fuse on EAP/g' \
        -e 's/the wildfly-camel subsystem installed/JBoss Fuse installed/g' \
        -e 's/standalone-full-camel.xml/standalone-full.xml/g' \
        -e '/Learn more/,+20d' \
+       -e 's/WildFly/EAP/g' \
     ${README}
