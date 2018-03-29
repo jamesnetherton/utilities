8ea6102b598bca0fba69293ad5fae2d1d41057cd
James Netherton <jamesnetherton@gmail.com>
2018-03-29 13:29:13 +0100

    Adjust regex for new naming

--- readme-productizer/productize.sh
+++ readme-productizer/productize.sh
@@ -2,11 +2,12 @@
 
 README=$1
 
-sed -i -e 's/WildFly Camel susbsystem/JBoss Fuse on EAP/g' \
-       -e 's/WildFly Camel susbsystem/JBoss Fuse on EAP/g' \
-       -e 's/WildFly Camel Subsystem/JBoss Fuse on EAP/g' \
-       -e 's/the EAP Camel subsystem/JBoss Fuse on EAP/g' \
-       -e 's/the wildfly-camel subsystem installed/JBoss Fuse installed/g' \
+sed -i -e 's/WildFly Camel susbsystem/Red Hat Fuse on EAP/g' \
+       -e 's/WildFly Camel susbsystem/Red Hat Fuse on EAP/g' \
+       -e 's/WildFly Camel Subsystem/Red Hat Fuse on EAP/g' \
+       -e 's/the EAP Camel subsystem/Red Hat Fuse on EAP/g' \
+       -e 's/the wildfly-camel subsystem installed/Red Hat Fuse installed/g' \
+       -e 's/JBoss Fuse/Red Hat Fuse/g' \
        -e 's/standalone-full-camel.xml/standalone-full.xml/g' \
        -e 's/WildFly/EAP/g' \
     ${README}
